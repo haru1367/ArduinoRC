11177322b303ba6408813493f8c133c15e7cc984
haru1367 <jsk03023@naver.com>
2024-05-14 10:54:43 +0900

    add Camera and gitignore

--- test/MOTOR/MOTOR.ino
+++ test/MOTOR/MOTOR.ino
@@ -1,7 +1,14 @@
-const int dcMotors[] = {};
+#define Motor1Pin1 3
+#define Motor1Pin2 5
+// #define Motor1Pin1 6
+// #define Motor1Pin1 9
+
+
+const int dcMotors[] = {Motor1Pin1, Motor1Pin2};
 const int forward[] = {HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW};
 const int backward[] = {LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH};
-const int STOP[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW}; 
+const int STOP[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+const int motorSpeed[] = {100, 200};
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,26 +22,26 @@ void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i < sizeof(dcMotors) / sizeof(dcMotors)[0]; i++)
   {
-    digitalWrite(dcMotors[i], forward[i]);
-    delay(1000);
+    analogWrite(i, motorSpeed[0]);
   }
+  delay(1000);
 
   for (int i = 0; i < sizeof(dcMotors) / sizeof(dcMotors)[0]; i++)
   {
-    digitalWrite(dcMotors[i], stop[i]);
-    delay(1000);
+    digitalWrite(dcMotors[i], STOP[i]);
   }
+  delay(1000);
    
   for (int i = 0; i < sizeof(dcMotors) / sizeof(dcMotors)[0]; i++)
   {
-    digitalWrite(dcMotors[i], backward[i]);
-    delay(1000);
+    analogWrite(i, motorSpeed[1]);
   }
+  delay(1000);
 
     for (int i = 0; i < sizeof(dcMotors) / sizeof(dcMotors)[0]; i++)
   {
-    digitalWrite(dcMotors[i], stop[i]);
-    delay(1000);
+    digitalWrite(dcMotors[i], STOP[i]);
   }
+    delay(1000);
 } 
 
